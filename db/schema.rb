3c2a3637fcec154dcda36b9b2f2427b8ec3cfd37
Kouta ASAI <scene.4913@gmail.com>
2015-08-01 16:11:21 +0900

    paperclipで画像ファイルをアップロードできるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801070312) do
+ActiveRecord::Schema.define(version: 20150801070811) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20150801070312) do
     t.datetime "date"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end

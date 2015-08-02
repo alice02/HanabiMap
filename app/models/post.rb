62249fc9c59569e671feccbfc51b0a7b71208665
Kouta ASAI <scene.4913@gmail.com>
2015-08-02 21:50:44 +0900

    postモデルにバリデーションをかけた

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
  	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
  	# ファイルの拡張子を指定（これがないとエラーが発生する）
  	validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+ 	validates_attachment :photo, presence: true
+ 	validates :name, :latitude, :longitude, presence: true
 end

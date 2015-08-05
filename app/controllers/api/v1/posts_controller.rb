ee315bc0bbbab13170b9fa6cb6a7188bd53468e1
Kouta ASAI <scene.4913@gmail.com>
2015-08-06 01:50:23 +0900

    api用のコントローラーを作った．

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::PostsController < ApplicationController
+
+	def index
+		render status: 200, json: Post.all
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			render status:200, json: { message: 'success' }
+		else
+			render status:200, json: { message: 'failure' }
+		end
+	end
+
+
+	private
+		def post_params
+			params.require(:post).permit(:name, :comment, :date, :latitude, :longitude, :photo)
+		end
 end
